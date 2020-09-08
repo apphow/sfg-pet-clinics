36e76c484eef7ce715e5468a5ef899773283735b
aprilhoward <apehow111@gmail.com>
2020-09-07 20:49:46 -0400

    Initial Project Add

---
+++ src/main/java/com/springframework/sfgpetclinics/SfgPetClinicsApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sfgpetclinics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/sfgpetclinics/SfgPetClinicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfgpetclinics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
